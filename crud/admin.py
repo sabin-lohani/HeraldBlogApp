34322966e8603744dbf7d9b084044d538cbf3024
Sabin Lohani <np03cs4s220104@heraldcollege.edu.np>
2023-08-02 08:05:03 +0545

    Admin site modifications

--- crud/admin.py
+++ crud/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from .models import Blog, Contacts, User, FooterContent, FaIcon, FooterLink
+
+admin.site.site_header = 'Herald BlogApp'
+admin.site.site_title = "Herald BlogApp"
+admin.site.index_title = 'Admin'
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "author", "title", "subheading", "description")
+    fields = ("title",)
+    list_editable = "title", "subheading",
+    search_fields = "title",
+
 # Register your models here.
 
 
-admin.site.register(Blog)
+admin.site.register(Blog, BlogAdmin)
 admin.site.register(Contacts)
 admin.site.register(User)
 admin.site.register(FooterContent)
