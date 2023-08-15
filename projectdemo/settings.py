2610c52aabffe0a78c16b65730f1cde8e6beec12
Sabin Lohani <np03cs4s220104@heraldcollege.edu.np>
2023-08-15 09:43:22 +0545

    integrated whitenoise

--- projectdemo/settings.py
+++ projectdemo/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'projectdemo.urls'
@@ -130,6 +131,7 @@ USE_TZ = True
 
 STATIC_URL = '/public/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/blog-post-media/'
 
